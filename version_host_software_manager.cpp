2697634dd91f3b178055f970038bfccc29b883d9
Gunnar Mills <gmills@us.ibm.com>
2017-03-08 15:00:08 -0600

    Add base support for PNOR code management
    This is the starting code needed to build the application.
    
    Change-Id: Ie2937ed6246b8646b9e5b4d09318bc4aa7b31c35
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

---
+++ version_host_software_manager.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "version_host_software_manager.hpp"
+
+namespace openpower
+{
+namespace software
+{
+namespace manager
+{
+
+} // namespace manager
+} // namespace software
+} // namepsace openpower
+
