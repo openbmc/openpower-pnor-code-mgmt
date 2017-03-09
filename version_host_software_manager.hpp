399bcf7987d202cd0c92ebb3327de838db4ff413
Gunnar Mills <gmills@us.ibm.com>
2017-03-09 14:42:47 -0600

    Add Software Activation Interface
    Change-Id: I82b16fc180ed8aa84a671a4173d74cca5c257fbf
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- version_host_software_manager.hpp
+++ version_host_software_manager.hpp
@@ -2,6 +2,7 @@
 
 #include <sdbusplus/bus.hpp>
 #include "xyz/openbmc_project/Software/Version/server.hpp"
+#include "xyz/openbmc_project/Software/Activation/server.hpp"
 
 namespace phosphor
 {
@@ -10,13 +11,16 @@ namespace software
 namespace manager
 {
 
+using VersionInherit = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Software::server::Version,
+    sdbusplus::xyz::openbmc_project::Software::server::Activation>;
+
 /** @class Version
  *  @brief OpenBMC version software management implementation.
  *  @details A concrete implementation for xyz.openbmc_project.Software.Version
  *  DBus API.
  */
-class Version : public sdbusplus::server::object::object<
-                sdbusplus::xyz::openbmc_project::Software::server::Version>
+class Version : public VersionInherit
 {
     public:
         /** @brief Constructs Version Software Manager
@@ -25,10 +29,8 @@ class Version : public sdbusplus::server::object::object<
          * @param[in] objPath   - The Dbus object path
          */
         Version(sdbusplus::bus::bus& bus,
-                const char* objPath) :
-                sdbusplus::server::object::object<
-                    sdbusplus::xyz::openbmc_project::Software::server::Version>
-                        (bus, objPath) {};
+                const char* objPath) : VersionInherit(
+                    bus, objPath) {};
 };
 
 } // namespace manager
