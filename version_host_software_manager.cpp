0376d9f754746d244379cb385ac1a3f3663919b5
Gunnar Mills <gmills@us.ibm.com>
2017-03-20 15:02:27 -0500

    Determine the host software id
    The id is determined by hashing the PNOR version.
    For this current story, openbmc/openbmc#1276, I assume the pnor
    partitions are located in /tmp/pnor. The PNOR version is read
    from pnor.toc
    
    Change-Id: Iddab7bb4ae5c350c60abf93057ddd997c97fa84c
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- version_host_software_manager.cpp
+++ version_host_software_manager.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <stdexcept>
+#include <phosphor-logging/log.hpp>
 #include "version_host_software_manager.hpp"
 
 namespace openpower
@@ -6,6 +12,60 @@ namespace software
 {
 namespace manager
 {
+
+using namespace phosphor::logging;
+
+const std::string Version::getVersion()
+{
+    constexpr auto versionKey = "version=";
+    constexpr auto versionKeySize = strlen(versionKey);
+    constexpr auto tocFilePath = "/tmp/pnor/pnor.toc";
+
+    std::string version{};
+    std::ifstream efile;
+    std::string line;
+    efile.exceptions(std::ifstream::failbit
+                     | std::ifstream::badbit
+                     | std::ifstream::eofbit);
+
+    // Too many GCC bugs (53984, 66145) to do this the right way...
+    try
+    {
+        efile.open(tocFilePath);
+        while (getline(efile, line))
+        {
+            if (line.compare(0, versionKeySize, versionKey) == 0)
+            {
+                version = line.substr(versionKeySize);
+                break;
+            }
+        }
+        efile.close();
+    }
+    catch (const std::exception& e)
+    {
+        log<level::ERR>("Error in reading Host pnor.toc file");
+    }
+
+    return version;
+}
+
+const std::string Version::getId(const std::string& version)
+{
+    std::stringstream hexId;
+
+    if (version.empty())
+    {
+        log<level::ERR>("Error Host version is empty");
+    }
+    else
+    {
+        // Only want 8 hex digits.
+        hexId << std::hex << ((std::hash<std::string> {}(
+                                   version)) & 0xFFFFFFFF);
+    }
+    return hexId.str();
+}
 
 } // namespace manager
 } // namespace software
