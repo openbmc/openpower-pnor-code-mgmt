f6483b07038f943b48530044a65f53df033de42b
Gunnar Mills <gmills@us.ibm.com>
2017-03-20 15:02:27 -0500

    Determine the host software id
    The id is determined by hashing the PNOR version.
    For this current story, openbmc/openbmc#1276, I assume the pnor
    partitions are located in /tmp/pnor. The PNOR version is read
    from pnor.toc
    
    Change-Id: Iddab7bb4ae5c350c60abf93057ddd997c97fa84c
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- version_host_software_manager.cpp
+++ version_host_software_manager.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <stdexcept>
 #include "version_host_software_manager.hpp"
 
 namespace openpower
@@ -6,6 +11,48 @@ namespace software
 {
 namespace manager
 {
+
+const std::string Version::getVersion()
+{
+    constexpr auto versionKey = "version=";
+    constexpr auto versionKeySize = strlen(versionKey);
+    std::string version{};
+    std::ifstream efile;
+    std::string line;
+    efile.open("/tmp/pnor/pnor.toc");
+
+    if (efile.fail())
+    {
+        throw std::runtime_error("Error opening pnor.toc file");
+    }
+
+    while (getline(efile, line))
+    {
+        if (line.compare(0, versionKeySize, versionKey) == 0)
+        {
+            // This line looks like:
+            // version=open-power-witherspoon-v1.14-89-ga42959a.
+            version = line.substr(versionKeySize);
+            break;
+        }
+    }
+    efile.close();
+    return version;
+}
+
+const std::string Version::getId(const std::string& version)
+{
+    std::stringstream hexId;
+
+    if (version.empty())
+    {
+        throw std::runtime_error("Host version is empty");
+    }
+
+    // Only want 8 hex digits.
+    hexId << std::hex << ((std::hash<std::string> {}(version)) & 0xFFFFFFFF);
+    return hexId.str();
+}
 
 } // namespace manager
 } // namespace software
