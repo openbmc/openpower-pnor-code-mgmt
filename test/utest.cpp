89a1fef464ae8420795d399c03dc5a5816b4763d
Gunnar Mills <gmills@us.ibm.com>
2017-03-27 21:49:16 -0500

    Add gtests
    Added gtests for getVersion() and getId()
    Resolves openbmc/openbmc#1276
    
    Change-Id: I10a824ae3da426e901defc34fa7f3c6c6a8496d3
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

---
+++ test/utest.cpp
@@ -0,0 +1,66 @@
+#include "version_host_software_manager.hpp"
+#include <gtest/gtest.h>
+#include <experimental/filesystem>
+#include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace openpower::software::manager;
+namespace fs = std::experimental::filesystem;
+
+constexpr auto masterDir = "/tmp";
+
+class VersionTest : public testing::Test
+{
+    protected:
+
+        virtual void SetUp()
+        {
+            char dir[50];
+            strcpy(dir, masterDir);
+            strcat(dir, "/versionXXXXXX");
+
+            auto path = mkdtemp(dir);
+            assert(path != nullptr);
+
+            _directory = path;
+
+        }
+
+        virtual void TearDown()
+        {
+            fs::remove_all(_directory);
+        }
+
+        std::string _directory;
+};
+
+/** @brief Make sure we correctly get the version from getVersion()*/
+TEST_F(VersionTest, TestGetVersion)
+{
+    auto tocFilePath = _directory + "/" + "pnor.toc";
+    auto version = "test-version";
+
+    std::ofstream file;
+    file.open(tocFilePath, std::ofstream::out);
+
+    file << "version=" << version;
+    file.close();
+
+    EXPECT_EQ(Version::getVersion(tocFilePath),version);
+}
+
+/** @brief Make sure we correctly get the Id from getId()*/
+TEST_F(VersionTest, TestGetId)
+{
+    std::stringstream hexId;
+    auto version = "test-id";
+
+    hexId << std::hex << ((std::hash<std::string> {}(
+                               version)) & 0xFFFFFFFF);
+
+    EXPECT_EQ(Version::getId(version),hexId.str());
+
+}
