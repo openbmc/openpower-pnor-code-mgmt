c4d2294060c43333fc955b238b5ba2be9c2952bc
Gunnar Mills <gmills@us.ibm.com>
2017-03-30 15:26:25 -0500

    Initial Download manager
    This is the basic download manager that implements
    xyz/openbmc_project/Common/TFTP.interface.yaml
    
    Change-Id: Ic38b9cb4b4fda0461fc259ed79854ed331c7702a
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

---
+++ download_manager.cpp
@@ -0,0 +1,13 @@
+#include "download_manager.hpp"
+
+namespace openpower
+{
+namespace software
+{
+namespace manager
+{
+
+} // namespace manager
+} // namespace software
+} // namepsace openpower
+
