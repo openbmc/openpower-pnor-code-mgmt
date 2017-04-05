3a1a6d343178db2e57d812d9514b549dcba2259f
Adriana Kobylak <anoo@us.ibm.com>
2017-04-05 16:42:13 -0500

    Activation: Create ActivationBlocksTransition
    Create ActivationBlocksTransition when the Activation property is
    set to Activating. Remove it when the property is set to anything else.
    
    Change-Id: I7a6a1921f040a2e738751de1a3a138be5869afda
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- activation.cpp
+++ activation.cpp
@@ -19,6 +19,29 @@ int Activation::handleActivationChangedSignal(sd_bus_message* msg,
 void Activation::handleActivationChanged(sdbusplus::message::message& msg,
                                          sd_bus_error* err)
 {
+    std::string key;
+    std::map<std::string, sdbusplus::message::variant<std::string>> msgData;
+    msg.read(key, msgData);
+    std::string value = sdbusplus::message::variant_ns::get<std::string>(
+                                msgData.begin()->second);
+
+    if (!value.compare(convertForMessage(
+                Activation::Activations::Activating).c_str()))
+    {
+        activationBlocksTransitions.insert(std::make_pair(
+                  versionId,
+                  std::make_unique<ActivationBlocksTransition>(
+                            busActivation,
+                            pathActivation)));
+    }
+    else
+    {
+        auto block = activationBlocksTransitions.find(versionId);
+        if (block != activationBlocksTransitions.end())
+        {
+            activationBlocksTransitions.erase(block);
+        }
+    }
 }
 
 } // namespace manager
