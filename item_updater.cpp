2ad8be1c1af3a80af20cf110b644a589a3835bee
Adriana Kobylak <anoo@us.ibm.com>
2017-04-05 16:16:52 -0500

    Activation: Save the version id
    Save the version id so that the path doesn't have to be parsed
    every time the version id is needed.
    
    Change-Id: Id89ab79bae521e5194cd38d46f73ed983ca200da
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- item_updater.cpp
+++ item_updater.cpp
@@ -62,7 +62,8 @@ int ItemUpdater::createActivation(sd_bus_message* msg,
                     versionId,
                     std::make_unique<Activation>(
                             updater->busItem,
-                            resp)));
+                            resp,
+                            std::move(versionId))));
         }
     }
     return 0;
