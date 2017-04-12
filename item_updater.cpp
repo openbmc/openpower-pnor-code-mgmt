c4d2c9f261e8527fc0c8637d84eccd3f3511b0a9
Saqib Khan <khansa@us.ibm.com>
2017-04-12 10:27:56 -0500

    ItemUpdater: Validate and extract PNOR squashfs image
    - Validate the tarball by checking its content.
    - Untar the tarball.
    - Set Activation to "Ready" if validation passed or
      else set it to "Invald".
    
    Resolves openbmc/openbmc#1356
    
    Change-Id: Ica73ae5604e74db11cd58c223d82265309ea76a6
    Signed-off-by: Saqib Khan <khansa@us.ibm.com>

--- item_updater.cpp
+++ item_updater.cpp
@@ -1,6 +1,13 @@
+#include <sys/wait.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string>
 #include <phosphor-logging/log.hpp>
 #include "config.h"
 #include "item_updater.hpp"
+#include "activation.hpp"
 
 namespace openpower
 {
@@ -9,7 +16,13 @@ namespace software
 namespace manager
 {
 
+// When you see server:: you know we're referencing our base class
+namespace server = sdbusplus::xyz::openbmc_project::Software::server;
+
 using namespace phosphor::logging;
+
+constexpr auto PNOR_SQUASHFS_IMAGE = "pnor.xz.squashfs";
+constexpr auto IMAGES_DIR = "/tmp/images/";
 
 int ItemUpdater::createActivation(sd_bus_message* msg,
                                   void* userData,
@@ -56,17 +69,123 @@ int ItemUpdater::createActivation(sd_bus_message* msg,
         }
 
         auto versionId = resp.substr(pos + 1);
+
+        // Determine the Activation state by processing the given tarball.
+        auto activationState = server::Activation::Activations::Invalid;
+        if (ItemUpdater::processImage(TARBALL_PATH, versionId) == 0)
+        {
+            activationState = server::Activation::Activations::Ready;
+        }
+
         if (updater->activations.find(versionId) == updater->activations.end())
         {
             updater->activations.insert(std::make_pair(
                     versionId,
                     std::make_unique<Activation>(
                             updater->busItem,
-                            resp)));
+                            resp,
+                            activationState)));
         }
     }
     return 0;
 }
+
+int ItemUpdater::processImage(const std::string& tarballFilePath,
+                              std::string& versionId)
+{
+    if (ItemUpdater::validateTarball(tarballFilePath) != 0)
+    {
+        log<level::ERR>("Failed to validate the tarball.");
+        return -1;
+    }
+     
+    if (ItemUpdater::unTAR(tarballFilePath, versionId) != 0)
+    {
+        log<level::ERR>("Failed to untar the tarball.");
+        return -1;
+    }
+
+    return 0;
+}
+
+int ItemUpdater::unTAR(const std::string& tarballFilePath,
+                       std::string& versionId)
+{
+    pid_t pid = fork();
+    if(pid < 0)
+    {
+        log<level::ERR>("fork() failed.");
+        return -1;
+    }
+    if(pid != 0)
+    {
+        wait(NULL); // wait for child process to join with parent
+    }
+    else
+    {
+        std::string command = "mkdir -p " + std::string(IMAGES_DIR) + 
+                               versionId.c_str() + " && tar -xvf " + 
+                               std::string(tarballFilePath.c_str()) + 
+                               " -C " + std::string(IMAGES_DIR) + 
+                               versionId.c_str();
+        execl("/bin/sh", "sh", "-c", command.c_str(), (char *)0);
+
+        // execl only returns on fail
+        log<level::ERR>("Failed to untar file",
+                        entry("FILENAME=%s", tarballFilePath));
+        return -1;
+    }
+    return 0;
+}
+
+
+int ItemUpdater::validateTarball(const std::string& tarballFilePath)
+{    
+    int link[2];
+    pid_t pid;
+    char output[4096];
+
+    if (pipe(link)==-1)
+    {
+        log<level::ERR>("Pipe failed.");
+        return -1;
+    }
+
+    if ((pid = fork()) < 0)
+    {
+        log<level::ERR>("fork() failed.");
+        return -1;
+    }
+    if(pid == 0)
+    {
+        // Child Process.
+        dup2 (link[1], STDOUT_FILENO);
+        close(link[0]);
+        close(link[1]);
+        execl("/bin/tar", "tar", "-t", "-f", tarballFilePath.c_str(), (char *)0);
+        return -1;
+    }
+    else
+    {
+        // Parent Process.
+        close(link[1]);
+        link[0] = read(link[0], output, sizeof(output));
+        std::string str(output);
+        if(str.find(PNOR_SQUASHFS_IMAGE) != std::string::npos)
+        {
+            // Found squashfs image in the tarball
+            return 0;
+        }
+        else
+        {
+            log<level::ERR>("Unable to find the squashfs image in tarball.");
+            return -1;
+        }
+        wait(NULL);
+    }
+    return -1;
+}
+
 
 } // namespace manager
 } // namespace software
