99c8c0e995bfe8bbdbb25705f8e44624fdf7c1a4
Adriana Kobylak <anoo@us.ibm.com>
2017-04-17 13:39:11 -0500

    Activation: Create ActivationBlocksTransition
    Create ActivationBlocksTransition when the Activation property is
    set to Activating. Remove it when the property is set to anything else.
    
    Change-Id: I7a6a1921f040a2e738751de1a3a138be5869afda
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- activation.cpp
+++ activation.cpp
@@ -7,18 +7,32 @@ namespace software
 namespace updater
 {
 
+namespace softwareServer = sdbusplus::xyz::openbmc_project::Software::server;
+
 auto Activation::activation(Activations value) ->
         Activations
 {
-    return sdbusplus::xyz::openbmc_project::Software::server::Activation::
-            activation(value);
+    if (value == softwareServer::Activation::Activations::Activating)
+    {
+        if (!activationBlocksTransition)
+        {
+            activationBlocksTransition =
+                      std::make_unique<ActivationBlocksTransition>(
+                                bus,
+                                path);
+        }
+    }
+    else
+    {
+        activationBlocksTransition.reset(nullptr);
+    }
+    return softwareServer::Activation::activation(value);
 }
 
 auto Activation::requestedActivation(RequestedActivations value) ->
         RequestedActivations
 {
-    return sdbusplus::xyz::openbmc_project::Software::server::Activation::
-            requestedActivation(value);
+    return softwareServer::Activation::requestedActivation(value);
 }
 
 } // namespace updater
