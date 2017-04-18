562267da8359e49d6a6db772caf6bf8d2324ce6b
Adriana Kobylak <anoo@us.ibm.com>
2017-04-18 13:55:44 -0500

    Activation: Write PNOR partitions to flash
    Create function to write the PNOR partitions tagged with the
    READWRITE flag from the read-only volume to the read-write one.
    
    Change-Id: Ic46a998fa22852b990e6030f61ba2338de146582
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- activation.hpp
+++ activation.hpp
@@ -83,6 +83,13 @@ class Activation : public ActivationInherit
 
         /** @brief Persistent ActivationBlocksTransition dbus object */
         std::unique_ptr<ActivationBlocksTransition> activationBlocksTransition;
+
+    private:
+        /** @brief Writes the PNOR partitions to flash
+         *
+         *  @return Success or error
+         */
+        int writePartition();
 };
 
 } // namespace updater
