a694487503e9a5269478fe7f375e21e3b861cb46
Adriana Kobylak <anoo@us.ibm.com>
2017-04-26 14:43:52 -0500

    Activation: Write PNOR partitions to flash
    Create function to write the PNOR partitions tagged with the
    READWRITE flag from the read-only volume to the read-write one.
    
    Change-Id: Ic46a998fa22852b990e6030f61ba2338de146582
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- activation.cpp
+++ activation.cpp
@@ -1,3 +1,7 @@
+#include <algorithm>
+#include <experimental/filesystem>
+#include <fstream>
+#include <sstream>
 #include "activation.hpp"
 #include "config.h"
 
@@ -8,7 +12,85 @@ namespace software
 namespace updater
 {
 
+namespace fs = std::experimental::filesystem;
 namespace softwareServer = sdbusplus::xyz::openbmc_project::Software::server;
+
+int Activation::writePartition()
+{
+    constexpr auto partitionKey = "partition";
+    constexpr auto rwFlag = "READWRITE";
+    constexpr auto rwFlagSize = strlen(rwFlag);
+
+    std::ifstream toc("/media/pnor-ro-" + versionId + "/pnor.toc");
+    toc.exceptions(std::ifstream::failbit
+                     | std::ifstream::badbit
+                     | std::ifstream::eofbit);
+    std::string line;
+
+    try
+    {
+        if (!toc.is_open())
+        {
+            return -1;
+        }
+        while (std::getline(toc, line))
+        {
+            // Partition line format: partitionXX=value1,value2,...
+            auto pos = line.find(partitionKey);
+            if (pos == std::string::npos)
+            {
+                continue;
+            }
+            // Replace the ',' delimiter with spaces for parsing
+            std::replace(line.begin(), line.end(), ',', ' ');
+            pos = line.find("=");
+            if (pos == std::string::npos)
+            {
+                return -1;
+            }
+            std::istringstream data(line.substr(pos + 1));
+            std::string name, startAddr, endAddr, flag;
+            // Partitions can have one or more flags
+            if (data >> name >> startAddr >> endAddr >> flag)
+            {
+                do
+                {
+                    // Only write the partitions with RW flag
+                    if (flag.compare(0, flag.size(), rwFlag, rwFlagSize) == 0)
+                    {
+                        fs::path source = "/media/pnor-ro-";
+                        source += versionId;
+                        source /= name;
+                        fs::path target = "/media/pnor-rw-";
+                        target += versionId;
+                        target /= name;
+                        fs::copy_file(source, target,
+                                fs::copy_options::overwrite_existing);
+                    }
+                } while(data >> flag);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        toc.close();
+    }
+    catch (const std::exception& e)
+    {
+        if (toc.eof())
+        {
+            // getline() reads beyond last line and sets the error bit,
+            // clear if that's the case.
+            toc.clear();
+            toc.close();
+            return 0;
+        }
+        return -1;
+    }
+
+    return 0;
+}
 
 auto Activation::activation(Activations value) ->
         Activations
@@ -49,6 +131,11 @@ auto Activation::requestedActivation(RequestedActivations value) ->
         method.append(ubimountServiceFile,
                       "replace");
         bus.call_noreply(method);
+        auto rc = writePartition();
+        if (rc != 0)
+        {
+            // Handle error
+        }
     }
     return softwareServer::Activation::requestedActivation(value);
 }
