0fa1875fcee3bb62f943b3fd33a82b5946c61489
Adriana Kobylak <anoo@us.ibm.com>
2017-05-14 16:13:00 -0500

    Activation: Point active image to requested image
    Once an image has been updated, point the current image to it.
    Closes: openbmc/openbmc#1278
    
    Change-Id: If21938283913d4b40cadf44952b2a530c9266812
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- activation.cpp
+++ activation.cpp
@@ -1,3 +1,4 @@
+#include <experimental/filesystem>
 #include "activation.hpp"
 #include "config.h"
 
@@ -8,6 +9,7 @@ namespace software
 namespace updater
 {
 
+namespace fs = std::experimental::filesystem;
 namespace softwareServer = sdbusplus::xyz::openbmc_project::Software::server;
 
 auto Activation::activation(Activations value) ->
@@ -34,7 +36,7 @@ auto Activation::activation(Activations value) ->
                 "StartUnit");
         method.append(squashfsMountServiceFile,
                       "replace");
-        bus.call_noreply(method);
+        bus.call(method);
     }
     else
     {
@@ -62,6 +64,46 @@ auto Activation::requestedActivation(RequestedActivations value) ->
         method.append(ubimountServiceFile,
                       "replace");
         bus.call_noreply(method);
+
+        // Check UBI volumes were created
+        if ((fs::exists(PNOR_PRSV)) &&
+            (fs::exists(PNOR_RW_PREFIX + versionId)))
+        {
+            if (!fs::exists(PNOR_ACTIVE_PATH))
+            {
+                fs::create_directories(PNOR_ACTIVE_PATH);
+            }
+
+            // If the RW or RO active links exist, remove them and create new
+            // ones pointing to the active version.
+            if (fs::exists(PNOR_RO_ACTIVE_PATH))
+            {
+                fs::remove(PNOR_RO_ACTIVE_PATH);
+            }
+            fs::create_directory_symlink(PNOR_RO_PREFIX + versionId,
+                    PNOR_RO_ACTIVE_PATH);
+            if (fs::exists(PNOR_RW_ACTIVE_PATH))
+            {
+                fs::remove(PNOR_RW_ACTIVE_PATH);
+            }
+            fs::create_directory_symlink(PNOR_RW_PREFIX + versionId,
+                    PNOR_RW_ACTIVE_PATH);
+
+            // There is only one preserved directory as it is not tied to a
+            // version, so just create the link if it doesn't exist already.
+            if (!fs::exists(PNOR_PRSV_ACTIVE_PATH))
+            {
+                fs::create_directory_symlink(PNOR_PRSV, PNOR_PRSV_ACTIVE_PATH);
+            }
+            
+            Activation::activation(
+                    softwareServer::Activation::Activations::Active);
+        }
+        else
+        {
+            Activation::activation(
+                    softwareServer::Activation::Activations::Failed);
+        }
     }
     return softwareServer::Activation::requestedActivation(value);
 }
