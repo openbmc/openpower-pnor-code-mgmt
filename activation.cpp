79d5a62914e6bada7ea610657a0af7e8db4f1189
Saqib Khan <khansa@us.ibm.com>
2017-06-07 14:37:36 -0500

    RedundancyPriority: Override set property function
    Create override function so that actions can be taken
    based on value that the property is being set to.
    
    Change-Id: I875fe8238718efb6b4d6dcd9f9bf26a8bc4998f1
    Signed-off-by: Saqib Khan <khansa@us.ibm.com>

--- activation.cpp
+++ activation.cpp
@@ -128,6 +128,11 @@ auto Activation::requestedActivation(RequestedActivations value) ->
     return softwareServer::Activation::requestedActivation(value);
 }
 
+uint8_t RedundancyPriority::priority(uint8_t value)
+{
+    return softwareServer::RedundancyPriority::priority(value);
+}
+
 } // namespace updater
 } // namespace software
 } // namespace openpower
