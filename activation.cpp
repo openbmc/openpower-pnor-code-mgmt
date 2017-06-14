4b7014b5df2cce6ff26ac084205d0b3e8ffe2ed8
Michael Tritz <mtritz@us.ibm.com>
2017-06-14 14:41:43 -0500

    Activation: Wait for service files to complete
    The Activation process calls a pair of service files upon which
    later steps are dependent. These units take some time to run, so
    the later steps would fail. This commit subscribes the Activation
    object to systemd signals and puts the later steps on hold until
    the service files have finished running.
    
    Additionally, the service file used to move the squashfs to flash
    is moved into the activation process, so not all images that the
    user downloads are saved to flash.
    
    Resolves openbmc/openbmc#1716
    
    Change-Id: Id3ecf6334e069f69c355f0c0e8901a93fd95d496
    Signed-off-by: Michael Tritz <mtritz@us.ibm.com>

--- activation.cpp
+++ activation.cpp
@@ -25,80 +25,116 @@ auto Activation::activation(Activations value) ->
     {
         softwareServer::Activation::activation(value);
 
-        if (!activationBlocksTransition)
+        if (squashfsLoaded == false && rwVolumesCreated == false)
         {
-            activationBlocksTransition =
-                      std::make_unique<ActivationBlocksTransition>(
-                                bus,
-                                path);
-        }
+            // If the squashfs image has not yet been loaded to pnor and the
+            // RW volumes have not yet been created, we need to start the
+            // service files for each of those actions.
 
-        constexpr auto ubimountService = "obmc-flash-bios-ubimount@";
-        auto ubimountServiceFile = std::string(ubimountService) +
-                                   versionId +
-                                   ".service";
-        auto method = bus.new_method_call(
-                SYSTEMD_BUSNAME,
-                SYSTEMD_PATH,
-                SYSTEMD_INTERFACE,
-                "StartUnit");
-        method.append(ubimountServiceFile,
-                      "replace");
-        bus.call_noreply(method);
-
-        // The ubimount service files attemps to create the RW and Preserved
-        // UBI volumes. If the service fails, the mount directories PNOR_PRSV
-        // and PNOR_RW_PREFIX_<versionid> won't be present. Check for the
-        // existence of those directories to validate the service file was
-        // successful, also for the existence of the RO directory where the
-        // image is supposed to reside.
-        if ((fs::exists(PNOR_PRSV)) &&
-            (fs::exists(PNOR_RW_PREFIX + versionId)) &&
-            (fs::exists(PNOR_RO_PREFIX + versionId)))
-        {
-            if (!fs::exists(PNOR_ACTIVE_PATH))
+            if (!activationBlocksTransition)
             {
-                fs::create_directories(PNOR_ACTIVE_PATH);
-            }
-
-            // If the RW or RO active links exist, remove them and create new
-            // ones pointing to the active version.
-            if (fs::exists(PNOR_RO_ACTIVE_PATH))
-            {
-                fs::remove(PNOR_RO_ACTIVE_PATH);
-            }
-            fs::create_directory_symlink(PNOR_RO_PREFIX + versionId,
-                    PNOR_RO_ACTIVE_PATH);
-            if (fs::exists(PNOR_RW_ACTIVE_PATH))
-            {
-                fs::remove(PNOR_RW_ACTIVE_PATH);
-            }
-            fs::create_directory_symlink(PNOR_RW_PREFIX + versionId,
-                    PNOR_RW_ACTIVE_PATH);
-
-            // There is only one preserved directory as it is not tied to a
-            // version, so just create the link if it doesn't exist already.
-            if (!fs::exists(PNOR_PRSV_ACTIVE_PATH))
-            {
-                fs::create_directory_symlink(PNOR_PRSV, PNOR_PRSV_ACTIVE_PATH);
-            }
-
-            // Set Redundancy Priority before setting to Active
-            if (!redundancyPriority)
-            {
-                redundancyPriority =
-                          std::make_unique<RedundancyPriority>(
+                activationBlocksTransition =
+                          std::make_unique<ActivationBlocksTransition>(
                                     bus,
                                     path);
             }
 
-            return softwareServer::Activation::activation(
-                    softwareServer::Activation::Activations::Active);
+            constexpr auto squashfsMountService =
+                    "obmc-flash-bios-squashfsmount@";
+            auto squashfsMountServiceFile = std::string(squashfsMountService) +
+                    versionId + ".service";
+            auto method = bus.new_method_call(
+                    SYSTEMD_BUSNAME,
+                    SYSTEMD_PATH,
+                    SYSTEMD_INTERFACE,
+                    "StartUnit");
+            method.append(squashfsMountServiceFile, "replace");
+            bus.call_noreply(method);
+
+            constexpr auto ubimountService = "obmc-flash-bios-ubimount@";
+            auto ubimountServiceFile = std::string(ubimountService) +
+                   versionId +
+                   ".service";
+            method = bus.new_method_call(
+                    SYSTEMD_BUSNAME,
+                    SYSTEMD_PATH,
+                    SYSTEMD_INTERFACE,
+                    "StartUnit");
+            method.append(ubimountServiceFile, "replace");
+            bus.call_noreply(method);
+
+            return value;
+        }
+        else if (squashfsLoaded == true && rwVolumesCreated == true)
+        {
+            // Only when the squashfs image is finished loading AND the RW
+            // volumes have been created do we proceed with activation.
+
+            squashfsLoaded = false;
+            rwVolumesCreated = false;
+
+            // The ubimount service files attemps to create the RW and Preserved
+            // UBI volumes. If the service fails, the mount dirs PNOR_PRSV
+            // and PNOR_RW_PREFIX_<versionid> won't be present. Check for the
+            // existence of those directories to validate the service file was
+            // successful, also for the existence of the RO directory where the
+            // image is supposed to reside.
+            if ((fs::exists(PNOR_PRSV)) &&
+                (fs::exists(PNOR_RW_PREFIX + versionId)) &&
+                (fs::exists(PNOR_RO_PREFIX + versionId)))
+            {
+                if (!fs::exists(PNOR_ACTIVE_PATH))
+                {
+                    fs::create_directories(PNOR_ACTIVE_PATH);
+                }
+
+                // If the RW or RO active links exist, remove them and create new
+                // ones pointing to the active version.
+                if (fs::exists(PNOR_RO_ACTIVE_PATH))
+                {
+                    fs::remove(PNOR_RO_ACTIVE_PATH);
+                }
+                fs::create_directory_symlink(PNOR_RO_PREFIX + versionId,
+                        PNOR_RO_ACTIVE_PATH);
+                if (fs::exists(PNOR_RW_ACTIVE_PATH))
+                {
+                    fs::remove(PNOR_RW_ACTIVE_PATH);
+                }
+                fs::create_directory_symlink(PNOR_RW_PREFIX + versionId,
+                        PNOR_RW_ACTIVE_PATH);
+
+                // There is only one preserved directory as it is not tied to a
+                // version, so just create the link if it doesn't exist already.
+                if (!fs::exists(PNOR_PRSV_ACTIVE_PATH))
+                {
+                    fs::create_directory_symlink(PNOR_PRSV,
+                            PNOR_PRSV_ACTIVE_PATH);
+                }
+
+                // Set Redundancy Priority before setting to Active
+                if (!redundancyPriority)
+                {
+                    redundancyPriority =
+                              std::make_unique<RedundancyPriority>(
+                                        bus,
+                                        path);
+                }
+
+                return softwareServer::Activation::activation(
+                        softwareServer::Activation::Activations::Active);
+            }
+            else
+            {
+                return softwareServer::Activation::activation(
+                        softwareServer::Activation::Activations::Failed);
+            }
         }
         else
         {
-            return softwareServer::Activation::activation(
-                    softwareServer::Activation::Activations::Failed);
+            // If either the squashfs image has not yet been loaded or the RW
+            // volumes have not yet been created, the activation process is
+            // ongoing, so we return "Activating" status.
+            return value;
         }
     }
     else
@@ -133,7 +169,43 @@ uint8_t RedundancyPriority::priority(uint8_t value)
     return softwareServer::RedundancyPriority::priority(value);
 }
 
+void Activation::unitStateChange(sdbusplus::message::message& msg)
+{
+    uint32_t newStateID {};
+    sdbusplus::message::object_path newStateObjPath;
+    std::string newStateUnit{};
+    std::string newStateResult{};
+
+    //Read the msg and populate each variable
+    msg.read(newStateID, newStateObjPath, newStateUnit, newStateResult);
+
+    auto squashfsMountServiceFile =
+            "obmc-flash-bios-squashfsmount@" + versionId + ".service";
+
+    auto ubimountServiceFile =
+            "obmc-flash-bios-ubimount@" + versionId + ".service";
+
+    if((newStateUnit == squashfsMountServiceFile) && (newStateResult == "done"))
+    {
+       squashfsLoaded = true;
+    }
+
+    if((newStateUnit == ubimountServiceFile) && (newStateResult == "done"))
+    {
+        rwVolumesCreated = true;
+    }
+
+    if((newStateUnit == squashfsMountServiceFile) ||
+            (newStateUnit == ubimountServiceFile))
+    {
+        Activation::activation(
+                softwareServer::Activation::Activations::Activating);
+    }
+
+
+    return;
+}
+
 } // namespace updater
 } // namespace software
 } // namespace openpower
-
