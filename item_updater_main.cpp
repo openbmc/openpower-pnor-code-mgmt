1c493c4369f565c25e8103c3b0f1ff56e62dec6a
Gunnar Mills <gmills@us.ibm.com>
2017-09-18 09:22:36 -0500

    Call updateFunctionalAssociation on symlink change
    Call updateFunctionalAssociation, to change the functional
    association to the new "running" PNOR image, on a symlink change.
    The symlink change occurs on chassis poweron.
    Look at the RO symlink, /var/lib/phosphor-software-manager/pnor/ro,
    for a symlink change.
    
    Resolves openbmc/openbmc#2279
    
    Change-Id: I2c673635af55da1c642e6d96ab6e12951b3a4fd3
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- item_updater_main.cpp
+++ item_updater_main.cpp
@@ -2,22 +2,42 @@
 #include <sdbusplus/server/manager.hpp>
 #include "config.h"
 #include "item_updater.hpp"
+#include <phosphor-logging/log.hpp>
+#include "watch.hpp"
 
 int main(int argc, char* argv[])
 {
+    using namespace openpower::software::updater;
     auto bus = sdbusplus::bus::new_default();
+
+    sd_event* loop = nullptr;
+    sd_event_default(&loop);
 
     // Add sdbusplus ObjectManager.
     sdbusplus::server::manager::manager objManager(bus, SOFTWARE_OBJPATH);
 
-    openpower::software::updater::ItemUpdater updater(bus, SOFTWARE_OBJPATH);
+    ItemUpdater updater(bus, SOFTWARE_OBJPATH);
 
     bus.request_name(BUSNAME_UPDATER);
-
-    while (true)
+    try
     {
-        bus.process_discard();
-        bus.wait();
+        openpower::software::updater::Watch watch(
+                loop,
+                std::bind(std::mem_fn(
+                                  &ItemUpdater::updateFunctionalAssociation),
+                          &updater,
+                          std::placeholders::_1));
+        bus.attach_event(loop, SD_EVENT_PRIORITY_NORMAL);
+        sd_event_loop(loop);
     }
+    catch (std::exception& e)
+    {
+        using namespace phosphor::logging;
+        log<level::ERR>(e.what());
+        return -1;
+    }
+
+    sd_event_unref(loop);
+
     return 0;
 }
