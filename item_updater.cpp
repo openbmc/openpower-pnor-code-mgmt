bd6fceb40609dac24036a8181015bc650eaa7f48
Gunnar Mills <gmills@us.ibm.com>
2017-09-20 12:51:28 -0500

    Create functional association on startup
    Look at /var/lib/phosphor-software-manager/pnor/ro
    to determine if there is a funcitonal image, if so
    create a functional association.
    
    Resolves openbmc/openbmc#2279
    
    Change-Id: I6e576e2e21a592a98d843eb45cdab8bba8f55192
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- item_updater.cpp
+++ item_updater.cpp
@@ -250,6 +250,13 @@ void ItemUpdater::processPNORImage()
                                      *this)));
         }
     }
+
+    // Look at the RO symlink to determine if there is a functional image
+    auto id = determineId(PNOR_RO_ACTIVE_PATH);
+    if (!id.empty())
+    {
+        updateFunctionalAssociation(std::string{SOFTWARE_OBJPATH} + '/' + id);
+    }
     return;
 }
 
@@ -530,6 +537,21 @@ void ItemUpdater::removeActiveAssociation(std::string path)
             ++iter;
         }
     }
+}
+
+std::string ItemUpdater::determineId(const std::string& symlinkPath)
+{
+    if (!fs::exists(symlinkPath))
+    {
+        return "";
+    }
+
+    auto target = std::string{fs::canonical(symlinkPath)};
+
+    // Get the image <id> from the symlink target
+    // for example /media/ro-2a1022fe
+    static const auto PNOR_RO_PREFIX_LEN = strlen(PNOR_RO_PREFIX);
+    return target.substr(PNOR_RO_PREFIX_LEN);
 }
 
 } // namespace updater
