dce4c6a1293cc67d94be6f5f8a8e4e521b72539f
Saqib Khan <khansa@us.ibm.com>
2017-10-22 11:29:07 -0500

    PNOR: Fix the delete implementation
    - Implement delete interface inside version class
    - As part of the activation process, the version from
      inside the image_manager is deleted and so is the
      version's tarfile from the image upload dir.
    
    Partially resolves openbmc/openbmc#2490
    
    Change-Id: Ie515cc01d5f154e6e55b9a3fb71d831730cd46f6
    Signed-off-by: Saqib Khan <khansa@us.ibm.com>

--- item_updater.cpp
+++ item_updater.cpp
@@ -135,14 +135,18 @@ void ItemUpdater::createActivation(sdbusplus::message::message& m)
                         extendedVersion,
                         activationState,
                         associations)));
-        versions.insert(std::make_pair(
-                            versionId,
-                            std::make_unique<Version>(
-                                bus,
-                                path,
-                                version,
-                                purpose,
-                                filePath)));
+        auto versionPtr = std::make_unique<Version>(
+                                  bus,
+                                  path,
+                                  version,
+                                  purpose,
+                                  filePath,
+                                  std::bind(&ItemUpdater::erase,
+                                          this,
+                                          std::placeholders::_1));
+        versionPtr->deleteObject =
+                std::make_unique<Delete>(bus, path, *versionPtr);
+        versions.insert(std::make_pair(versionId, std::move(versionPtr)));
     }
     return;
 }
@@ -239,14 +243,20 @@ void ItemUpdater::processPNORImage()
             }
 
             // Create Version instance for this version.
+            auto versionPtr = std::make_unique<Version>(
+                                      bus,
+                                      path,
+                                      version,
+                                      purpose,
+                                      "",
+                                      std::bind(&ItemUpdater::erase,
+                                              this,
+                                              std::placeholders::_1));
+            versionPtr->deleteObject =
+                        std::make_unique<Delete>(bus, path, *versionPtr);
             versions.insert(std::make_pair(
-                                id,
-                                std::make_unique<Version>(
-                                     bus,
-                                     path,
-                                     version,
-                                     purpose,
-                                     "")));
+                                    id,
+                                    std::move(versionPtr)));
         }
         else if (0 == iter.path().native().compare(0, PNOR_RW_PREFIX_LEN,
                                                       PNOR_RW_PREFIX))
@@ -349,6 +359,7 @@ void ItemUpdater::reset()
 
     if (reply.is_method_error())
     {
+        log<level::ERR>("Error in Mapper call while deleting pnor-prsv");
         elog<InternalFailure>();
     }
 
@@ -476,9 +487,11 @@ void ItemUpdater::erase(std::string entryId)
         log<level::ERR>(("Error: Failed to find version " + entryId + \
                          " in item updater versions map." \
                          " Unable to remove.").c_str());
-        return;
     }
-    versions.erase(entryId);
+    else
+    {
+        versions.erase(entryId);
+    }
 
     // Removing entry in activations map
     auto ita = activations.find(entryId);
@@ -487,26 +500,22 @@ void ItemUpdater::erase(std::string entryId)
         log<level::ERR>(("Error: Failed to find version " + entryId + \
                          " in item updater activations map." \
                          " Unable to remove.").c_str());
-        return;
     }
-    activations.erase(entryId);
+    else
+    {
+        activations.erase(entryId);
+    }
+    return;
 }
 
 void ItemUpdater::deleteAll()
 {
-    std::vector<std::string> deletableActivations;
-
     for (const auto& activationIt : activations)
     {
         if (!isVersionFunctional(activationIt.first))
         {
-            deletableActivations.push_back(activationIt.first);
+            ItemUpdater::erase(activationIt.first);
         }
-    }
-
-    for (const auto& deletableIt : deletableActivations)
-    {
-        ItemUpdater::erase(deletableIt);
     }
 
     // Remove any remaining pnor-ro- or pnor-rw- volumes that do not match
