9fbb87c44f00833a35ad577b7ddc124335310515
Adriana Kobylak <anoo@us.ibm.com>
2018-10-09 15:26:44 -0500

    msl_verify: Initial commit
    Create a new binary that checks the msl (minimum ship level)
    of the PNOR and logs an error message if the version on the
    system is older. The msl can be specified via a config flag.
    
    Change-Id: I6f477400f7a8cf56557bd0caf5d6e08d73320028
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

---
+++ msl_verify.cpp
@@ -0,0 +1,165 @@
+#include "config.h"
+
+#include <experimental/filesystem>
+#include <fstream>
+#include <phosphor-logging/log.hpp>
+#include <regex>
+
+namespace openpower
+{
+namespace software
+{
+namespace msl
+{
+
+namespace fs = std::experimental::filesystem;
+using namespace phosphor::logging;
+
+struct Version_t
+{
+    uint32_t major;
+    uint32_t minor;
+    uint32_t rev;
+};
+
+/**
+ * @brief Compare the versions provided
+ * @param[in] a - The first version to compare
+ * @param[in] b - The second version to compare
+ * @return 0 if a <= b, -1 otherwise.
+ */
+static int compare(const Version_t& a, const Version_t& b)
+{
+    if (a.major > b.major)
+    {
+        return -1;
+    }
+    else if (a.major < b.major)
+    {
+        return 0;
+    }
+
+    if (a.minor > b.minor)
+    {
+        return -1;
+    }
+    else if (a.minor < b.minor)
+    {
+        return 0;
+    }
+
+    if (a.rev > b.rev)
+    {
+        return -1;
+    }
+    else if (a.rev < b.rev)
+    {
+        return 0;
+    }
+
+    return 0;
+}
+
+/**
+ * @brief Parse the version parts into a struct
+ * @details Version format follows a git tag convention: vX.Y[.Z]
+ *          Reference:
+ *          https://github.com/open-power/op-build/blob/master/openpower/package/VERSION.readme
+ * @param[in] version - The version string to be parsed
+ * @param[out] versionStruct - The struct to be populated
+ */
+static void parse(const std::string& version, Version_t& versionStruct)
+{
+    std::smatch match;
+
+    // Match for vX.Y.Z
+    std::regex regex{"v([0-9]+)\\.([0-9]+)\\.([0-9]+)", std::regex::extended};
+
+    if (!std::regex_search(version, match, regex))
+    {
+        // Match for vX.Y
+        std::regex regexShort{"v([0-9]+)\\.([0-9]+)", std::regex::extended};
+        if (!std::regex_search(version, match, regexShort))
+        {
+            log<level::ERR>("Unable to parse PNOR version",
+                            entry("VERSION=%s", version.c_str()));
+            return;
+        }
+    }
+    else
+    {
+        // Populate Z
+        versionStruct.rev = std::stoi(match[3]);
+    }
+    versionStruct.major = std::stoi(match[1]);
+    versionStruct.minor = std::stoi(match[2]);
+}
+
+static std::string getFunctionalPnorVersion()
+{
+    fs::path versionPath(PNOR_RO_ACTIVE_PATH);
+    versionPath /= PNOR_VERSION_PARTITION;
+    if (!fs::is_regular_file(versionPath))
+    {
+        return {};
+    }
+
+    std::ifstream versionFile(versionPath);
+    std::string versionStr;
+    std::getline(versionFile, versionStr);
+
+    return versionStr;
+}
+
+/**
+ * @brief Verify that the current PNOR version meets the min ship level (msl)
+ */
+static void verify()
+{
+    auto min = std::string{PNOR_MSL};
+
+    if (std::string(PNOR_MSL).empty())
+    {
+        return;
+    }
+
+    if (!fs::exists(PNOR_RO_ACTIVE_PATH))
+    {
+        return;
+    }
+
+    auto actual = getFunctionalPnorVersion();
+    if (actual.empty())
+    {
+        return;
+    }
+
+    Version_t min_t = {0, 0, 0};
+    parse(min, min_t);
+
+    Version_t actual_t = {0, 0, 0};
+    parse(actual, actual_t);
+
+    auto rc = compare(min_t, actual_t);
+    if (rc != 0)
+    {
+        log<level::ERR>(
+            "PNOR Mininum Ship Level NOT met",
+            entry("MIN_VERSION=%s", min.c_str()),
+            entry("ACTUAL_VERSION=%s", actual.c_str()),
+            entry("VERSION_PURPOSE=%s",
+                  "xyz.openbmc_project.Software.Version.VersionPurpose.Host"));
+    }
+
+    return;
+}
+} // namespace msl
+} // namespace software
+} // namespace openpower
+
+int main(int argc, char** argv)
+{
+    openpower::software::msl::verify();
+
+    return 0;
+}
