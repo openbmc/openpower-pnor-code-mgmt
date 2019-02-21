950e1c3b456f2ee6d2711a7e8384b4418be4346c
Lei YU <mine260309@gmail.com>
2019-02-21 15:52:53 +0800

    Refactor: Split Activation into common and ubi
    Activations has a few functions coupled with ubi, split them into
    ubi/activation_ubi
    
    * Keep common code in activation.
    * Make start/finishActivation() pure virtual.
    * Move ubi specific code into ubi.
    * Move ubiVolumnCreated into ubi.
    * Make validateSignature() not inline, otherwise it gets compile error.
    
    Tested: On the last commit of the patch series, run code update and
            factory reset on Witherspoon and all work fine.
    
    Change-Id: I7c8a0de6b0be4b1e9814ea75fd802a014b4aacfc
    Signed-off-by: Lei YU <mine260309@gmail.com>

---
+++ ubi/activation_ubi.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "activation.hpp"
+
+namespace openpower
+{
+namespace software
+{
+namespace updater
+{
+
+class RedundancyPriorityUbi : public RedundancyPriority
+{
+  public:
+    /** @brief Constructs RedundancyPriorityUbi.
+     *
+     *  @param[in] bus    - The Dbus bus object
+     *  @param[in] path   - The Dbus object path
+     *  @param[in] parent - Parent object.
+     *  @param[in] value  - The redundancyPriority value
+     */
+    RedundancyPriorityUbi(sdbusplus::bus::bus& bus, const std::string& path,
+                          Activation& parent, uint8_t value) :
+        RedundancyPriority(bus, path, parent, value)
+    {
+    }
+    virtual ~RedundancyPriorityUbi() = default;
+
+    /** @brief Overloaded Priority property set function
+     *
+     *  @param[in] value - uint8_t
+     *
+     *  @return Success or exception thrown
+     */
+    uint8_t priority(uint8_t value) override;
+};
+
+/** @class ActivationUbi
+ *  @brief OpenBMC activation software management implementation.
+ *  @details A concrete implementation for
+ *  xyz.openbmc_project.Software.Activation DBus API.
+ */
+class ActivationUbi : public Activation
+{
+  public:
+    /** @brief Constructs Activation Software Manager
+     *
+     * @param[in] bus    - The Dbus bus object
+     * @param[in] path   - The Dbus object path
+     * @param[in] parent - Parent object.
+     * @param[in] versionId  - The software version id
+     * @param[in] extVersion - The extended version
+     * @param[in] activationStatus - The status of Activation
+     * @param[in] assocs - Association objects
+     */
+    ActivationUbi(sdbusplus::bus::bus& bus, const std::string& path,
+                  ItemUpdater& parent, std::string& versionId,
+                  std::string& extVersion,
+                  sdbusplus::xyz::openbmc_project::Software::server::
+                      Activation::Activations activationStatus,
+                  AssociationList& assocs) :
+        Activation(bus, path, parent, versionId, extVersion, activationStatus,
+                   assocs)
+    {
+    }
+    virtual ~ActivationUbi() = default;
+
+    /** @brief Overloaded Activation property setter function
+     *
+     *  @param[in] value - One of Activation::Activations
+     *
+     *  @return Success or exception thrown
+     */
+    Activations activation(Activations value) override;
+
+    RequestedActivations
+        requestedActivation(RequestedActivations value) override;
+
+  private:
+    /** @brief Tracks whether the read-only & read-write volumes have been
+     *created as part of the activation process. **/
+    bool ubiVolumesCreated = false;
+
+    void unitStateChange(sdbusplus::message::message& msg) override;
+    void startActivation() override;
+    void finishActivation() override;
+};
+
+} // namespace updater
+} // namespace software
+} // namespace openpower
