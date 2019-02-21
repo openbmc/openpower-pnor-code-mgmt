f979717ab7cfcfa8e779868719f5a7b76390bf1d
Lei YU <mine260309@gmail.com>
2019-02-21 17:47:05 +0800

    Static layout: Read PNOR VERSION using pflash
    Use pflash to read VERSION partition to get the PNOR version
    Add code to parse pflash's VERSION partition string.
    
    Tested: On Romulus verify that the version and extended version are
            retrieved correctly.
    
    Change-Id: Ia053c1683a5a969be0773d251cb88c4c5c9b6c60
    Signed-off-by: Lei YU <mine260309@gmail.com>

--- static/item_updater_static.cpp
+++ static/item_updater_static.cpp
@@ -2,12 +2,67 @@
 
 #include "item_updater_static.hpp"
 
+#include "activation.hpp"
+#include "version.hpp"
+
+#include <filesystem>
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/log.hpp>
+#include <string>
+
+namespace utils
+{
+
+template <typename... Ts>
+std::string concat_string(Ts const&... ts)
+{
+    std::stringstream s;
+    ((s << ts << " "), ...) << std::endl;
+    return s.str();
+}
+
+// Helper function to run pflash command
+template <typename... Ts>
+std::string pflash(Ts const&... ts)
+{
+    std::array<char, 512> buffer;
+    std::string cmd = concat_string("pflash", ts...);
+    std::stringstream result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"),
+                                                  pclose);
+    if (!pipe)
+    {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+    {
+        result << buffer.data();
+    }
+    return result.str();
+}
+
+std::string getPNORVersion()
+{
+    // Read the VERSION partition skipping the first 4K
+    auto r = pflash("-P", "VERSION", "-r", "/dev/stderr", "--skip=4096",
+                    "2>&1 > /dev/null");
+    return r;
+}
+
+} // namespace utils
+
 namespace openpower
 {
 namespace software
 {
 namespace updater
 {
+// When you see server:: you know we're referencing our base class
+namespace server = sdbusplus::xyz::openbmc_project::Software::server;
+namespace fs = std::filesystem;
+
+using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+using namespace phosphor::logging;
 
 std::unique_ptr<Activation> ItemUpdaterStatic::createActivationObject(
     const std::string& path, const std::string& versionId,
@@ -36,6 +91,52 @@ bool ItemUpdaterStatic::validateImage(const std::string& path)
 
 void ItemUpdaterStatic::processPNORImage()
 {
+    auto fullVersion = utils::getPNORVersion();
+
+    const auto& [version, extendedVersion] = Version::getVersions(fullVersion);
+    auto id = Version::getId(version);
+
+    auto activationState = server::Activation::Activations::Active;
+    if (version.empty())
+    {
+        log<level::ERR>("Failed to read version",
+                        entry("VERSION=%s", fullVersion.c_str()));
+        activationState = server::Activation::Activations::Invalid;
+    }
+
+    if (extendedVersion.empty())
+    {
+        log<level::ERR>("Failed to read extendedVersion",
+                        entry("VERSION=%s", fullVersion.c_str()));
+        activationState = server::Activation::Activations::Invalid;
+    }
+
+    auto purpose = server::Version::VersionPurpose::Host;
+    auto path = fs::path(SOFTWARE_OBJPATH) / id;
+    AssociationList associations = {};
+
+    if (activationState == server::Activation::Activations::Active)
+    {
+        // Create an association to the host inventory item
+        associations.emplace_back(std::make_tuple(ACTIVATION_FWD_ASSOCIATION,
+                                                  ACTIVATION_REV_ASSOCIATION,
+                                                  HOST_INVENTORY_PATH));
+
+        // Create an active association since this image is active
+        createActiveAssociation(path);
+    }
+
+    // Create Version instance for this version.
+    auto versionPtr = std::make_unique<Version>(
+        bus, path, *this, id, version, purpose, "",
+        std::bind(&ItemUpdaterStatic::erase, this, std::placeholders::_1));
+    versionPtr->deleteObject = std::make_unique<Delete>(bus, path, *versionPtr);
+    versions.insert(std::make_pair(id, std::move(versionPtr)));
+
+    if (!id.empty())
+    {
+        updateFunctionalAssociation(id);
+    }
 }
 
 void ItemUpdaterStatic::reset()
--- version.cpp
+++ version.cpp
@@ -96,6 +96,41 @@ std::map<std::string, std::string>
     return keys;
 }
 
+std::pair<std::string, std::string>
+    Version::getVersions(const std::string& versionPart)
+{
+    // versionPart contains strings like below:
+    // open-power-romulus-v2.2-rc1-48-g268344f-dirty
+    //     buildroot-2018.11.1-7-g5d7cc8c
+    //     skiboot-v6.2
+    std::istringstream iss(versionPart);
+    std::string line;
+    std::string version;
+    std::stringstream ss;
+    std::string extendedVersion;
+
+    if (!std::getline(iss, line))
+    {
+        elog<InvalidArgument>(Argument::ARGUMENT_NAME("Version"),
+                              Argument::ARGUMENT_VALUE(version.c_str()));
+    }
+    version = line;
+
+    while (std::getline(iss, line))
+    {
+        // Each line starts with a tab, let's trim it
+        line.erase(line.begin(),
+                   std::find_if(line.begin(), line.end(),
+                                [](int c) { return !std::isspace(c); }));
+        ss << line << ',';
+    }
+    extendedVersion = ss.str();
+
+    // Erase the last ','
+    extendedVersion.pop_back();
+    return {version, extendedVersion};
+}
+
 void Delete::delete_()
 {
     if (parent.eraseCallback)
