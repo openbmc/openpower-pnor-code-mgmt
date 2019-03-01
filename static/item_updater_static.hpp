ea467bd23ba3c7ced9b1eb1793327fefd78f9c1b
Lei YU <mine260309@gmail.com>
2019-03-01 16:03:53 +0800

    test: Add sources of static PNOR
    Add sources in static dir to test.
    It generates a link error about duplcated GardReset::reset().
    
    Then an issue is found that GardReset::~GardReset() is not virtual,
    this will casuse the destructor not calling its super class' dtor.
    
    To fix the above link error, add GardReset[Ubi|Static] to make
    ubi/static specific implementation in its own class.
    
    Tested: Verify the test build works fine.
    
    Change-Id: I41bd4d522e8b9c4053283c0cf4373807ef4f2d1c
    Signed-off-by: Lei YU <mine260309@gmail.com>

--- static/item_updater_static.hpp
+++ static/item_updater_static.hpp
@@ -9,6 +9,23 @@ namespace software
 namespace updater
 {
 
+class GardResetStatic : public GardReset
+{
+  public:
+    GardResetStatic(sdbusplus::bus::bus& bus, const std::string& path) :
+        GardReset(bus, path)
+    {
+    }
+
+    virtual ~GardResetStatic() = default;
+
+  protected:
+    /**
+     * @brief GARD factory reset - clears the PNOR GARD partition.
+     */
+    void reset() override;
+};
+
 /** @class ItemUpdaterStatic
  *  @brief Manages the activation of the host version items for static layout
  */
@@ -19,7 +36,7 @@ class ItemUpdaterStatic : public ItemUpdater
         ItemUpdater(bus, path)
     {
         processPNORImage();
-        gardReset = std::make_unique<GardReset>(bus, GARD_PATH);
+        gardReset = std::make_unique<GardResetStatic>(bus, GARD_PATH);
         volatileEnable = std::make_unique<ObjectEnable>(bus, volatilePath);
 
         // Emit deferred signal.
