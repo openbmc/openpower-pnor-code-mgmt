4c955c31d96d4e3781abd9e9f9cf5c6aa1adfd56
Andrew Geissler <geissonator@yahoo.com>
2020-05-16 13:32:22 -0500

    remove un-needed use statement
    The latest yocto flags an error, indicating the redeclaration of
    "using ActivationInherit::activation;". The code does not match the
    doxygen and it is declared twice. The use statement does not appear to
    be needed so just remove it all.
    
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>
    Change-Id: I849b52fad61a96190195ffa64c4aca20c9bedbc8

--- activation.hpp
+++ activation.hpp
@@ -178,12 +178,6 @@ class Activation : public ActivationInherit
     }
     virtual ~Activation() = default;
 
-    /** @brief Activation property get function
-     *
-     *  @returns One of Activation::Activations
-     */
-    using ActivationInherit::activation;
-
     /** @brief Overloaded requestedActivation property setter function
      *
      *  @param[in] value - One of Activation::RequestedActivations
@@ -234,12 +228,6 @@ class Activation : public ActivationInherit
 
     /** @brief Used to subscribe to dbus systemd signals **/
     sdbusplus::bus::match_t systemdSignals;
-
-    /** @brief activation status property get function
-     *
-     * @returns Activations - The activation value
-     */
-    using ActivationInherit::activation;
 
     /**
      * @brief Determine the configured image apply time value
