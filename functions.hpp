9a8c4aba08223a575870e9c3fc19d32b260527c6
Adriana Kobylak <anoo@us.ibm.com>
2021-06-14 17:42:40 +0000

    mmc: Add update-bios-attr-table subcommand
    Add the update-bios-attr-table subcommand to update the bios attribute
    table with the host firmware well-known names based on system type. The
    system type is provided by Entity Manager, and this new subcommand uses
    some of the existing infrastructure that the process-host-firmware
    subcommand has to determine the system type.
    
    Add a new service file to run this new subcommand so that it can block
    waiting for the entity manager interface to appear on D-Bus.
    
    Subsequent commands will add parsing the JSON file to build the bios
    attribute string and set the bios attribute property.
    
    Change-Id: Iacee975e273bae562d2d42cd8b642b9d0744a121
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- functions.hpp
+++ functions.hpp
@@ -45,5 +45,9 @@ bool maybeCall(
 std::shared_ptr<void> processHostFirmware(
     sdbusplus::bus::bus&, std::map<std::string, std::vector<std::string>>,
     std::filesystem::path, ErrorCallbackType, sdeventplus::Event&);
+std::shared_ptr<void>
+    updateBiosAttrTable(sdbusplus::bus::bus&,
+                        std::map<std::string, std::vector<std::string>>,
+                        sdeventplus::Event&);
 } // namespace process_hostfirmware
 } // namespace functions
