cb3de14651bdfa99eaf7221db3a4804559282121
Rashmica Gupta <rashmica@linux.ibm.com>
2024-07-05 15:50:40 +1000

    generate-tar: Update private signing key
    Updating from 1024 bits to 4096 bits for a more secure key
    
    Change-Id: Ia73b7dcf280d729ed23e3e1113c257c68e979d25
    Signed-off-by: Rashmica Gupta <rashmica@linux.ibm.com>

--- test/test_signature.cpp
+++ test/test_signature.cpp
@@ -65,7 +65,7 @@ class SignatureTest : public testing::Test
         command("echo \"pnor.xz.squashfs file \" > " + pnorFile);
 
         std::string pkeyFile = extractPath.string() + "/" + "private.pem";
-        command("openssl genrsa  -out " + pkeyFile + " 2048");
+        command("openssl genrsa  -out " + pkeyFile + " 4096");
 
         std::string pubkeyFile = extractPath.string() + "/" + "publickey";
         command("openssl rsa -in " + pkeyFile + " -outform PEM " +
